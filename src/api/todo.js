921f80838c7191386277688e8876d80153c96b6c
MikolaLvivUA <mikola.shidlovsky@gmail.com>
2020-03-23 23:52:14 +0200

    my-first-serverless local - functions with APIGatewayRouting

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,8 +1,8 @@
-module.exports.handler = (event, ctx, done) => {
+module.exports.todo = (event, ctx, done) => {
   done(null, {
     statusCode: 200,
     body: JSON.stringify({data: {
       id: 1, name: 'clean up', status: 'open'
     }})
   })
-}
+};
--- src/api/todos.js
+++ src/api/todos.js
@@ -1,10 +1,10 @@
-module.exports.handler = (event, ctx, done) => {
+module.exports.todos = (event, ctx, done) => {
   const todos = [
     {id: 1, name: 'clean up', status: 'open'},
     {id: 1, name: 'cook', status: 'done'}
-  ]
+  ];
   done(null, {
     statusCode: 200,
     body: JSON.stringify({data: todos})
   })
-}
+};
