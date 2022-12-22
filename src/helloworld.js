57e292306f083abc4de47b8ebeb7366b565dcd03
blakesmithmn <blake.smith.mn@gmail.com>
2022-12-22 16:05:24 -0600

    hello world!

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,3 +1,17 @@
-module.exports.handler = (evt, ctx, done) => {
-  
-}
+// event , context, done
+// done follows the NODE style response stuff
+// context is meta information 
+// event is the payload - whatever was sent to the lambda
+'use strict';
+
+module.exports.handler = (event, context, callback) => {
+    // const response = {
+    //     statusCode: 200,
+    //     body: JSON.stringify({
+    //         message: 'Hello World',
+    //         input: event,
+    //     }),
+    // };
+
+    callback(null, 'hello');
+};
